57ed61d31fa7aaf0b9d15280d013b433efc2a16b
Petr Petukhov <kotov0991@gmail.com>
2018-05-23 08:20:29 +0300

    starting the project

---
+++ app/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<meta property="og:image" content="img/opengraph.jpg">
+
+	<link rel="shortcut icon" href="img/favicon/favicon.ico" type="image/x-icon">
+	<link rel="apple-touch-icon" href="img/favicon/apple-touch-icon.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="img/favicon/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="img/favicon/apple-touch-icon-114x114.png">
+	
+	<!-- Chrome, Firefox OS and Opera -->
+	<meta name="theme-color" content="#000">
+	<!-- Windows Phone -->
+	<meta name="msapplication-navbutton-color" content="#000">
+	<!-- iOS Safari -->
+	<meta name="apple-mobile-web-app-status-bar-style" content="#000">
+
+	<title>МПК</title>
+
+	<link rel="stylesheet" href="css/styles.css">
+
+</head>
+
+<body>
+	<!-- intro -->
+	<section class="intro">
+		<!--  Video is muted & autoplays  -->
+		<video class="intro__video-bg" autoplay loop muted>
+			<source src="video/21.mp4" type="video/mp4">
+		</video>
+		<div class="container"></div>
+	</section>
+	<!-- intro end -->
+
+	<!-- Jquery с облака и замена в случае отсутствия доступа к облаку  -->
+	<script src="http://code.jquery.com/jquery-3.3.1.min.js"></script>
+	<script>window.jQuery || document.write('<script src="libs/jquery/dist/jquery.min.js"><\/script>')</script>
+
+	<script src="js/scripts.js"></script>
+
+</body>
+</html>
